f6e2e1284bb5d623eb541d652cfa8d10510e1451
William Moon <william-moon@lambdastudents.com>
2021-07-08 14:37:45 -0700

    react app

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
 const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 if (process.env.NODE_ENV === 'production') {
     console.log('this means this code is deployed')
@@ -9,6 +16,10 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000
 
 console.log('port is -->', PORT)
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 server.get('/api', (req, res) => {
     res.json({message: `${process.env.COHORT}`})
