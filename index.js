42b4e6a4ae5c80a886019ce64b72842ffd7a30bb
William Moon <william-moon@lambdastudents.com>
2021-07-08 14:11:08 -0700

    heroku

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const server = express()
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('this means this code is deployed')
+}
+
+const PORT = process.env.PORT || 5000
+
+console.log('port is -->', PORT)
+
+server.get('/api', (req, res) => {
+    res.json({message: `${process.env.COHORT}`})
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
